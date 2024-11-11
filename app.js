83750d25fec86c0c507630f94caa7bdbadc59977
aadarsh-kawatra <thekawatra@gmail.com>
2024-11-11 23:19:13 +0530

    server started

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.set("view engine", "ejs");
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});

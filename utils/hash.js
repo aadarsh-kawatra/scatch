d6e84e1ec90067a71e08d844c8646e8cecc1f56b
aadarsh-kawatra <thekawatra@gmail.com>
2024-11-13 01:17:47 +0530

    export fix

--- utils/hash.js
+++ utils/hash.js
@@ -1,5 +1,7 @@
-export async function hashValue(val) {
+async function hashValue(val) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(val, salt);
   return hash;
 }
+
+module.exports = { hashValue };

b946603957ac3e347f01f31f508f406685850983
aadarsh-kawatra <thekawatra@gmail.com>
2024-11-13 00:28:18 +0530

    routers setup

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+
 const db = require("./config/connect");
+const ownerRouter = require("./routes/owner-router");
+const userRouter = require("./routes/user-router");
+const productRouter = require("./routes/product-router");
 
 const app = express();
 app.use(express.json());
@@ -12,9 +16,9 @@ app.set("view engine", "ejs");
 
 const port = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/owner", ownerRouter);
+app.use("/user", userRouter);
+app.use("/product", productRouter);
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
